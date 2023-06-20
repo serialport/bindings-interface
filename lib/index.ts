5a64e5c3aeabfb76ecd92d0f6337555359c7bbd0
Gareth Hancock <64541249+GazHank@users.noreply.github.com>
2023-06-20 17:43:25 +0100

    Add friendlyName and lowLatency to interfaces
    Signed-off-by: Gareth Hancock <64541249+GazHank@users.noreply.github.com>

--- lib/index.ts
+++ lib/index.ts
@@ -11,6 +11,7 @@ export interface PortInfo {
   locationId: string | undefined
   productId: string | undefined
   vendorId: string | undefined
+  friendlyName: string | undefined
 }
 
 export interface OpenOptions {
@@ -56,6 +57,7 @@ export interface PortStatus {
   cts: boolean
   dsr: boolean
   dcd: boolean
+  lowLatency: boolean
 }
 
 /**
